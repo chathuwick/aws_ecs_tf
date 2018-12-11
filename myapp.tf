1f4d1e2af7306900082bba4bc289da51813060cf
chathuwick <35885787+chathuwick@users.noreply.github.com>
2018-12-11 13:19:44 +1300

    Update myapp.tf

--- myapp.tf
+++ myapp.tf
@@ -3,6 +3,7 @@ data "template_file" "myapp-task-definition-template" {
   template               = "${file("templates/app.json.tpl")}"
   vars {
     REPOSITORY_URL = "${replace("${aws_ecr_repository.ccprepo.repository_url}", "https://", "")}"
+    APP_VERSION = "${var.MYAPP_VERSION}"
   }
 }
 
